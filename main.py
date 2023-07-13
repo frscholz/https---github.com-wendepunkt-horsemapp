f81d193aa4468535467c551041e503634faa2dd2
UBC Student <fscholz@students.cs.ubc.ca>
2023-07-13 10:37:13 +0200

    Fixed some bugs

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+from pyodide.ffi.wrappers import add_event_listener
+from js import document
 import pandas as pd
 import panel as pn
-from datetime import datetime
-import io
 from io import StringIO
-from pyscript import Element
-from pyodide_http import patch_all
-patch_all()
 
 result = pd.DataFrame()
 
@@ -65,12 +62,14 @@ def submit_handler(event = None):
         addRow(form_values)
         display(f"{form_values['name']} added succesfully", target="message")
 
-def reset_handler(event = None):
-    if event:
-        form_values = {
-            "name": "", "email": "", "phone": "", "street": "", "city": "", "state": "", "zip": "", "country": "", "website": ""
-        }
-        display(f"Reset", target="message")         
+def reset_handler():
+    form_values = {
+        "name": "", "email": "", "phone": "", "street": "", "city": "", "state": "", "zip": "", "country": "", "website": ""
+    }
+    document.getElementById('name').value = ''
+
+def setup_handler():
+    document.getElementById("startup").value = 'Ready'
         
 # Map event handlers to elements
 
@@ -81,36 +80,32 @@ Element("street").element.oninput = street_input_handler
 Element("city").element.oninput = city_input_handler
 Element("state").element.oninput = state_input_handler
 Element("zip").element.oninput = zip_input_handler
-Element("country").element.oninput = country_handler
+add_event_listener(document.getElementById("country"), "change", country_handler)
 Element("website").element.oninput = website_input_handler
 Element("form").element.onsubmit = submit_handler
-Element("form").element.onreset = reset_handler
-
-# Functions
+add_event_listener(document.getElementById("download"), "change", setup_handler)
 
 # function to manually add rows to the result
 def addRow():
     global result
     # add check to see if form_values is empty
     result = result.append(form_values, ignore_index = True)
-    message = Element("message")
     reset_handler()
-    message.write("Row successfully added")
+    display("Row successfully added", target="message")
     return result
 
 # write df to paragraph
 def updateTable():
     sheet = Element("sheet")
     sheet.clear()
-    sheet.element.innerHTML = result.to_html(class="table")
-    # display(result, target="sheet", append=False)
+    sheet.element.innerHTML = result.to_html()
 
 # download csv
 def get_csv():
     global result
     return io.BytesIO(result.to_csv().encode())
 
-# Panel widgets
-pn.extension(design='material')
 file_download_csv = pn.widgets.FileDownload(filename="data.csv", callback=get_csv, button_type="primary")    
+success = pn.pane.Alert("Ready", alert_type="success")
 pn.Column(file_download_csv).servable(target="download")
+pn.Column(success).servable(target="startup")
